5e474ffad3b1500c0780f2057945255596ba1e2b
Konstantin Koltypanov <k.koltypanov@gmail.com>
2024-12-10 15:40:35 +0300

    chore: изменен тест

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,5 @@ import App from './App';
 test('renders greeting message', () => {
   render(<App />);
   const linkElement = screen.getByText(/Это тестовый проект/i);
-  // const linkElement = screen.getByText(/Это тестовый проект/i);
   expect(linkElement).toBeInTheDocument();
 });
